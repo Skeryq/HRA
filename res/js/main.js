f44a6575004918c21105b659a028c8dc3ff37832
SkolaVWA <dominikvoloscuk123@gmail.com>
2025-03-06 09:25:18 +0100

    ss

--- res/js/main.js
+++ res/js/main.js
@@ -6,7 +6,8 @@ class Player {
         this.w = w;
         this.h = h;
         this.c = c;
-        this.v = 5;
+        this.v = 2;
+        this.bv = 2;
     }
 
     draw(ctx){
@@ -15,14 +16,42 @@ class Player {
     }
 
     update(keys) {
-        if(keys["w"]) this.y -= this.v;
-        if(keys["s"]) this.y += this.v;
-        if(keys["a"]) this.x -= this.v;
-        if(keys["d"]) this.x += this.v;
+        if(keys["w"]) {
+            if (this.y - this.v > 0) {
+            this.y -= this.v;
+            }
+        }
+        if(keys["s"]) {
+            if (this.y + this.h + this.v < gameCanvas.height){
+                this.y += this.v;
+            }
+        }
+        if(keys["a"]) {
+            if (this.x - this.v > 0) {
+            this.x -= this.v;
+            }
+        }
+        if(keys["d"]) {
+            if (this.x + this.w + this.v < gameCanvas.width) {
+            this.x += this.v;
+        }
+        }   
+        if(keys[" "]) {
+            this.v = 6;
+    } else{
+        this.v = this.bv;
     }
+        if(keys["e"] && (this.w + 1 <= 250 || this.h + 1 <= 250)){
+            this.w += 2;
+            this.h += 2;
+        }
+        if(keys["q"] && (this.w + 1 >= 10 || this.h + 1 >= 10)){
+            this.w -= 2;
+            this.h -= 2;
+        }
 }
-
-const myPlayer = new Player(10, 10, 50, 50, "black");
+}
+const myPlayer = new Player(10, 10, 50, 50, "red");
 
 const gameCanvas = document.getElementById("gameCanvas");
 const ctx = gameCanvas.getContext("2d");
@@ -45,7 +74,24 @@ const gameLoop = () => {
 
     myPlayer.update(keys);
     myPlayer.draw(ctx);
+
+    ctx.fillStyle = testObject.c;
+    ctx.fillRect(testObject.x, testObject.y, testObject.w, testObject.h);
 requestAnimationFrame(gameLoop);
 }
 
 requestAnimationFrame(gameLoop);
+
+let testObject = {
+    x: 100,
+    y: 200,
+    w: 100,
+    h: 100,
+    c: "blue"
+}
+
+const checkCollision = (object1, object2) => {
+if (object1.x + object1.w > object2.x && object1.x < object2.x + object2.w && object1.y + object1.h > object2.y && object1.y < object2.y + object2.h){
+    console.log("Collision");
+}
+}
